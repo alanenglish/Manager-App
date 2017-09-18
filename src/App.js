21c7c5b35e2f5edcc7cec274c467e40acf88788f
Alan English <aenglish@manorhillcapital.com>
2017-09-18 11:07:12 -0500

    npm install firebase, import firebase, copy firebase config into app

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBsfO8EAinU7qVezhvO5UEbmZYYtX-EAmw',
+      authDomain: 'manager-d6b5a.firebaseapp.com',
+      databaseURL: 'https://manager-d6b5a.firebaseio.com',
+      projectId: 'manager-d6b5a',
+      storageBucket: 'manager-d6b5a.appspot.com',
+      messagingSenderId: '120756716101'
+    };
+    firebase.initializeApp(config);
+  }
+  
   render() {
     return (
       <Provider store={createStore(reducers)}>
