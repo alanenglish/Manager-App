2517c91df6a4cf269efd9e3954454e878a8f015d
Alan English <aenglish@manorhillcapital.com>
2017-09-25 19:56:49 -0500

    add style to modal, fix error where i set transparent equal to visible, add correct props, add card section and button to terminate employee, add logic to show confirm component model if terminate button is clicked

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -4,9 +4,11 @@ import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave, employeeClear } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
+  state = { showModal: false };
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -33,6 +35,7 @@ class EmployeeEdit extends Component {
     return (
       <Card>
         <EmployeeForm />
+        
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Save Changes
@@ -44,6 +47,18 @@ class EmployeeEdit extends Component {
             Text Schedule
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+            Terminate Employee
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+        >
+          Are you sure you want to terminate this employee?
+        </Confirm>
       </Card>
     );
   }
--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -4,16 +4,19 @@ import { CardSection } from './CardSection';
 import { Button } from './Button';
 
 const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  const { containerStyle, textStyle, cardSectionStyle } = styles;
+
   return (
     <Modal
-      transparent={visible}
+      visible={visible}
+      transparent
       animationType="slide"
-      // android requred to pass in onRequestClose, so we pass in empty function
+      // android required to pass in onRequestClose, so we pass in empty function
       onRequestClose={() => {}}
     >
-      <View>
-        <CardSection>
-          <Text>{children}</Text>
+      <View style={containerStyle}>
+        <CardSection style={cardSectionStyle}>
+          <Text style={textStyle}>{children}</Text>
         </CardSection>
 
         <CardSection>
@@ -23,6 +26,24 @@ const Confirm = ({ children, visible, onAccept, onDecline }) => {
       </View>
     </Modal>
   );
+};
+
+const styles = {
+  cardSectionStyle: {
+    justifyContent: 'center'
+  },
+  textStyle: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    lineHeight: 40
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
+  }
 };
 
 export { Confirm };
